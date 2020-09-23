7cfc7b7ab32146e3333c0a22e3e7031310f92e6f
tommios <tommios.net@gmail.com>
2020-09-23 15:26:25 +0300

    Changed styles for TodoForm

--- client/src/components/forms/TodoForm/style.js
+++ client/src/components/forms/TodoForm/style.js
@@ -1,19 +1,8 @@
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(1),
-            width: theme.spacing(8),
-            height: theme.spacing(16),
-        },
-    },
-    view: {
-        // width: "100%", // Fix IE 11 issue.
-        width: theme.spacing(56),
-        margin: "auto",
+    form: {
+        width: "100%", // Fix IE 11 issue.
         marginTop: theme.spacing(3),
     },
     submit: {
